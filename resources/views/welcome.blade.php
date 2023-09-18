282b31d07b1f99bbe225250824ee19a67de75de9
baezdev <baezdev@gmail.com>
2023-09-17 18:35:45 -0600

    add: componentes en home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,42 +1,7 @@
-<html>
-
-<head>
-    <meta name="viewport" content="width=divice-width, initial-scale=1.0" />
-    <meta name="author" content="Miguel_Ambrosi" />
-    <meta name="descrition" content="Pagina resposiva" />
-    <title>Prueba</title>
-    <link rel="stylesheet" href="/assets/images/logo.png" />
-    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
-</head>
-
-<body>
-    <nav>
-        <div class="container">
-            <div class="navbar">
-                <a href="/">
-                    <h2>SPASSSIO</h2>
-                </a>
-                <ul class="navbar__links">
-                    <li><a href="#" class="navbar__link">Inicio</a></li>
-                    <li><a href="#" class="navbar__link">Servicios</a></li>
-                    <li><a href="#" class="navbar__link">Testimonios</a></li>
-                    <li><a href="#" class="navbar__link">Paquetes</a></li>
-                    <li><a href="#" class="navbar__link">Beneficios</a></li>
-                    <li><a href="#" class="navbar__link">Nosotros</a></li>
-                    <li><a href="#" class="button button-primary">Iniciar Sesión</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <header class="header">
-        <div class="container">
-            <div class="header__text">
-                <h1>SPASSSIO</h1>
-                <h2 class="header__title">El mejor <span>SPASSSIO</span> para hacer ejercicio</h2>
-            </div>
-        </div>
-    </header>
-</body>
-
-</html>
+<x-layout>
+  {{-- <x-slot:title>
+    Custom Title
+  </x-slot> --}}
+  <x-navbar />
+  <x-home.header />
+</x-layout>
