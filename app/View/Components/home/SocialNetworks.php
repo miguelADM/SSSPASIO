24b8405bcb8086d38873e29d3330e6958223a9e5
baezdev <baezdev@gmail.com>
2023-09-21 23:58:06 -0600

    add: redes sociales

---
+++ app/View/Components/home/SocialNetworks.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\View\Components\home;
+
+use Illuminate\View\Component;
+
+class SocialNetworks extends Component
+{
+    public $networks = [
+        ['name' => 'facebook', 'url' => 'https://www.facebook.com/profile.php?id=100063713006920', 'color' => '#1877F2'],
+        ['name' => 'youtube', 'url' => 'https://www.youtube.com/channel/UC1FoqPUNzsfBCY0aVJ2MBDw?app=desktop', 'color' => '#FF0000'],
+        ['name' => 'telegram', 'url' => 'https://msng.link/o?spasssio=tg', 'color' => '#0088CC'],
+        ['name' => 'instagram', 'url' => 'https://www.instagram.com/spasssio/?igshid=YmMyMTA2M2Y%3D', 'color' => '#f600a8'],
+        ['name' => 'x', 'url' => 'https://twitter.com/spasssio?t=wjk9fCKU954LDVGezrrw4Q&s=08', 'color' => '#000000'],
+        ['name' => 'whatsapp', 'url' => 'https://api.whatsapp.com/send?phone=5215512583151&text=Hola,%20vengo%20de%20la%20p%C3%A1gina%20web%20quisiera%20saber%20m%C3%A1s%20sobre%20Spasssio', 'color' => '#25D366'],
+    ];
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        $socialNetworks = $this->networks;
+
+        return view('components.home.social-networks', compact('socialNetworks'));
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
   {{-- Todo: Agregar slot para darle titulo a la pagina --}}
   <x-ui.navbar />
   <x-home.header />
+  <x-home.social-networks />
   <x-home.services />
   <x-home.benefits />
   <x-home.plans />
