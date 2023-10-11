920ca11ef8de011c588c895a444b7591ecb234c1
baezdev <baezdev@gmail.com>
2023-10-11 16:41:39 -0600

    fix: links del inicio de administrador

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -4,7 +4,7 @@
 <x-layouts.admin-layout>
   <section class="admin__options">
     @foreach ($adminOptions as $option)
-      <a href="{{ $option['href'] }}" class="admin__option" style="border-color: {{ $option['color'] }};">
+      <a href="{{ 'admin/' . $option['href'] }}" class="admin__option" style="border-color: {{ $option['color'] }};">
         <div class="admin__option-text">
           <h4>{{ $option['text'] }}</h4>
           <span>5</span>
