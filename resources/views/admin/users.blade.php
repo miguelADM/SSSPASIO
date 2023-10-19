56694eb95bb78c0a66c53e42e467ba9e901cf7f3
baezdev <baezdev@gmail.com>
2023-10-18 19:23:10 -0600

    fix: datos en linea

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,67 +1,3 @@
-@php
-
-  function getAllUsers()
-  {
-      return $users = DB::table('users')->get();
-  }
-
-  function getProfileUser($userId): array
-  {
-      $profile = DB::table('profiles')
-          ->where('user_id', $userId)
-          ->get();
-      $profileData = [
-          'name' => $profile[0]->nombre_usu ?? null,
-          'lastname' => $profile[0]->apellidos_usu ?? null,
-          'membership' => $profile[0]->tipo_membresia ?? null,
-      ];
-
-      return $profileData;
-  }
-
-  function getIllness($illnessId)
-  {
-      $illnesse = DB::table('enfermedades')
-          ->where('id', $illnessId)
-          ->get();
-
-      return $illnesse[0]->nombre ?? null;
-  }
-
-  function getWorkingGroup($groupId)
-  {
-      $group = DB::table('grupos_trabajo')
-          ->where('GRUPO_ID', $groupId)
-          ->get();
-
-      return $group[0]->GRUPO_NOMBRE ?? null;
-  }
-
-  function getMappedUsers($users)
-  {
-      $mappedUsers = [];
-      foreach ($users as $user) {
-          $profileData = getProfileUser($user->id);
-
-          $mappedUsers[] = [
-              'id' => $user->id,
-              'username' => $user->name,
-              'name' => $profileData['name'],
-              'lastname' => $profileData['lastname'],
-              'membership' => $profileData['membership'],
-              'illness' => getIllness($user->enfermedad_id),
-              'working-group' => getWorkingGroup($user->grupo_id),
-              'email' => $user->email,
-          ];
-      }
-
-      return $mappedUsers;
-  }
-
-  $allUsers = getAllUsers();
-  $users = getMappedUsers($allUsers);
-
-@endphp
 <x-layouts.admin-layout>
   <h1>Usuarios</h1>
   <div class="button-container">
@@ -80,38 +16,116 @@
           <div class="divTableCell">Salud</div>
           <div class="divTableCell"></div>
         </div>
-        @foreach ($users as $user)
-          <div class="divTableRow">
-            <div class="divTableCell">{{ $user['username'] }}</div>
-            <div class="divTableCell">
-              @if ($user['name'] != null && $user['lastname'] != null)
-                <span>{{ $user['name'] }} {{ $user['lastname'] }}</span>
-              @else
-                <span>Anonimo</span>
-              @endif
-            </div>
-            <div class="divTableCell">{{ $user['email'] }}</div>
-            <div class="divTableCell">
-              @if ($user['working-group'] != null)
-                {{ $user['working-group'] }}
-              @else
-                Sin grupo
-              @endif
-            </div>
-            <div class="divTableCell">{{ $user['membership'] }}</div>
-            <div class="divTableCell">{{ $user['illness'] }}</div>
-            <div class="divTableCell relative">
-              <button class="table__options" type="button" data-id="{{ $user['id'] }}">
-                <img src="{{ asset('assets/icons/admin/options-vertical.svg') }}" alt="icono de opciones"
-                  loading="lazy">
+        {{-- row --}}
+        <div class="divTableRow">
+          <div class="divTableCell">Spasssio0001</div>
+          <div class="divTableCell">
+            Kazahura Miller
+          </div>
+          <div class="divTableCell">usuario@mail.com</div>
+          <div class="divTableCell">
+            UNIVERSIDAD NEZA
+          </div>
+          <div class="divTableCell">Premium</div>
+          <div class="divTableCell">Sindrome metabólico</div>
+          <div class="divTableCell relative">
+            <button class="table__options" type="button" data-id="1">
+              <img src="{{ asset('assets/icons/admin/options-vertical.svg') }}" alt="icono de opciones" loading="lazy">
+            </button>
+            <div class="table__options-menu">
+              <button class="edit">
+                <img src="{{ asset('assets/icons/admin/edit.svg') }}" alt="icono de editar" loading="lazy">
               </button>
-              <div class="table__options-menu">
-                <button>Eliminar</button>
-                <button>Editar</button>
-              </div>
+              <button class="delete">
+                <img src="{{ asset('assets/icons/admin/round-delete.svg') }}" alt="icono de eliminar" loading="lazy">
+              </button>
             </div>
           </div>
-        @endforeach
+        </div>
+        {{-- row --}}
+        {{-- row --}}
+        <div class="divTableRow">
+          <div class="divTableCell">Spasssio0001</div>
+          <div class="divTableCell">
+            Kazahura Miller
+          </div>
+          <div class="divTableCell">usuario@mail.com</div>
+          <div class="divTableCell">
+            UNIVERSIDAD NEZA
+          </div>
+          <div class="divTableCell">Premium</div>
+          <div class="divTableCell">Sindrome metabólico</div>
+          <div class="divTableCell relative">
+            <button class="table__options" type="button" data-id="1">
+              <img src="{{ asset('assets/icons/admin/options-vertical.svg') }}" alt="icono de opciones" loading="lazy">
+            </button>
+            <div class="table__options-menu">
+              <button class="edit">
+                <img src="{{ asset('assets/icons/admin/edit.svg') }}" alt="icono de editar" loading="lazy">
+              </button>
+              <button class="delete">
+                <img src="{{ asset('assets/icons/admin/round-delete.svg') }}" alt="icono de eliminar" loading="lazy">
+              </button>
+            </div>
+          </div>
+        </div>
+        {{-- row --}}
+        {{-- row --}}
+        <div class="divTableRow">
+          <div class="divTableCell">Spasssio0001</div>
+          <div class="divTableCell">
+            Kazahura Miller
+          </div>
+          <div class="divTableCell">usuario@mail.com</div>
+          <div class="divTableCell">
+            UNIVERSIDAD NEZA
+          </div>
+          <div class="divTableCell">Premium</div>
+          <div class="divTableCell">Sindrome metabólico</div>
+          <div class="divTableCell relative">
+            <button class="table__options" type="button" data-id="1">
+              <img src="{{ asset('assets/icons/admin/options-vertical.svg') }}" alt="icono de opciones" loading="lazy">
+            </button>
+            <div class="table__options-menu">
+              <button class="edit">
+                <img src="{{ asset('assets/icons/admin/edit.svg') }}" alt="icono de editar" loading="lazy">
+              </button>
+              <button class="delete">
+                <img src="{{ asset('assets/icons/admin/round-delete.svg') }}" alt="icono de eliminar" loading="lazy">
+              </button>
+            </div>
+          </div>
+        </div>
+        {{-- row --}}
+        {{-- row --}}
+        <div class="divTableRow">
+          <div class="divTableCell">Spasssio0001</div>
+          <div class="divTableCell">
+            Kazahura Miller
+          </div>
+          <div class="divTableCell">usuario@mail.com</div>
+          <div class="divTableCell">
+            UNIVERSIDAD NEZA
+          </div>
+          <div class="divTableCell">Premium</div>
+          <div class="divTableCell">Sindrome metabólico</div>
+          <div class="divTableCell relative">
+            <button class="table__options" type="button" data-id="1">
+              <img src="{{ asset('assets/icons/admin/options-vertical.svg') }}" alt="icono de opciones" loading="lazy">
+            </button>
+            <div class="table__options-menu">
+              <button class="edit">
+                <img src="{{ asset('assets/icons/admin/edit.svg') }}" alt="icono de editar" loading="lazy">
+              </button>
+              <button class="delete">
+                <img src="{{ asset('assets/icons/admin/round-delete.svg') }}" alt="icono de eliminar"
+                  loading="lazy">
+              </button>
+            </div>
+          </div>
+        </div>
+        {{-- row --}}
+
       </div>
     </div>
   </section>
