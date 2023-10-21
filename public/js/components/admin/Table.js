5ddf84aa3abb2937ac236e4674075f5dacdb649c
baezdev <baezdev@gmail.com>
2023-10-21 00:14:09 -0600

    add: abrir y cerrar opciones para borrar o editar registro

--- public/js/components/admin/Table.js
+++ public/js/components/admin/Table.js
@@ -1,17 +1,27 @@
 export function table() {
   const options_buttons = document.querySelectorAll(".table__options");
 
-  options_buttons.forEach((button) => {
+  const handleClickOptionsButton = (event) => {
+    const button = event.currentTarget;
     const options_menu = button.parentElement.querySelector(".table__options-menu");
+    const previus_menu = document.querySelector(".show__options");
 
-    button.addEventListener("click", (e) => {
-      const previus_menu = document.querySelector(".show__options");
-
+    if (!options_menu.classList.contains("show__options")) {
       if (previus_menu) {
         previus_menu.classList.remove("show__options");
       }
+      options_menu.classList.add("show__options");
+    }
+    else {
+      options_menu.classList.remove("show__options");
+    }
 
-      options_menu.classList.toggle("show__options");
-    })
+  }
+
+  options_buttons.forEach((button) => {
+    button.addEventListener("click", handleClickOptionsButton)
   })
+
 }
+
+
