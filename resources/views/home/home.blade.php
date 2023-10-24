b46371b039068f81fc1adb28b75dd2d7ad68e5ed
baezdev <baezdev@gmail.com>
2023-10-23 20:02:38 -0600

    add: blog de inicio de usuario

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -57,5 +57,61 @@
         <img src="{{ asset('assets/images/home/encuesta.png') }}" alt="Encuesta">
       </div>
     </article>
+
+    {{-- Blog --}}
+
+    <article class="home__blog">
+      <h2>Nuestro Blog</h2>
+      <div class="home__blog-container">
+        <div class="home__blog-card">
+          <div class="home__blog-content">
+            <img src="{{ asset('assets/images/home/1.webp') }}" alt="Blog">
+            <div class="home__blog-text">
+              <h5>Algo Más</h5>
+              <p>¿Conoces a alguien que no suelta su teléfono celular? Mantente al tanto de los riesgos del uso excesivo
+                del celular.
+              </p>
+            </div>
+          </div>
+          <a href="">Saber más</a>
+        </div>
+
+        <div class="home__blog-card">
+          <div class="home__blog-content">
+            <img src="{{ asset('assets/images/home/enterate.webp') }}" alt="Blog">
+            <div class="home__blog-text">
+              <h5>Entérate</h5>
+              <p>Conoce las noticias más reelevantes que pueden influir en tu salud.
+              </p>
+            </div>
+          </div>
+          <a href="">Saber más</a>
+        </div>
+
+        <div class="home__blog-card">
+          <div class="home__blog-content">
+            <img src="{{ asset('assets/images/home/2.webp') }}" alt="Blog">
+            <div class="home__blog-text">
+              <h5>Motivación</h5>
+              <p>Aprende a como mantener la motivación necesaria para darle un cambio a tu vida.
+              </p>
+            </div>
+          </div>
+          <a href="">Saber más</a>
+        </div>
+
+        <div class="home__blog-card">
+          <div class="home__blog-content">
+            <img src="{{ asset('assets/images/home/tips.webp') }}" alt="Blog">
+            <div class="home__blog-text">
+              <h5>Tips</h5>
+              <p>Descubre los tips más importantes para mejorar y mantener tus resultados.
+              </p>
+            </div>
+          </div>
+          <a href="">Saber más</a>
+        </div>
+      </div>
+    </article>
   </section>
 </x-layouts.user-layout>
