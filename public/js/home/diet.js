d944db3e1b53585ff3a8cd9f5472cbb3e4bc9c9a
baezdev <baezdev@gmail.com>
2023-11-04 16:40:15 -0600

    add: pagina de recomendacion nutricional

---
+++ public/js/home/diet.js
@@ -0,0 +1,102 @@
+async function getRecipes() {
+    const response = await fetch('/data/recipes.json');
+    const data = await response.json();
+
+    return data;
+}
+
+const recipes = await getRecipes();
+
+const getRecipeById = (id) => {
+    return recipes[id];
+}
+
+const getRandomNumber = (max) => {
+    return Math.floor(Math.random() * max);
+}
+const getRandomRecipes = () => {
+    const max = recipes.length
+
+    return [
+        recipes[getRandomNumber(max)],
+        recipes[getRandomNumber(max)],
+        recipes[getRandomNumber(max)]
+    ]
+}
+
+const nutritional__list = document.querySelector('.nutritional__list');
+const random_recipes_button = document.querySelector('#random-recipes');
+
+function createRecipeCard(recipe) {
+    const recipe_card = document.createElement('li');
+    recipe_card.classList.add('nutritional__recipe');
+    recipe_card.style.backgroundImage = `url(${recipe.image})`
+    recipe_card.setAttribute('data-id', recipe.id);
+
+    const recipe_text = document.createElement('div');
+    recipe_text.classList.add('nutritional__recipe-text');
+    recipe_text.innerHTML = `
+        <h5>${recipe.title}</h5>
+        <p></p>
+        <span>${recipe.description}</span>`
+
+    recipe_card.appendChild(recipe_text);
+    return recipe_card
+}
+
+function generateRandomRecipes() {
+    const randomRecipes = getRandomRecipes();
+    randomRecipes.forEach(recipe => {
+        const recipe_card = createRecipeCard(recipe);
+        nutritional__list.appendChild(recipe_card);
+    })
+
+    handleChangeRecipe()
+}
+
+generateRandomRecipes()
+
+random_recipes_button.addEventListener('click', () => {
+    nutritional__list.innerHTML = '';
+    generateRandomRecipes();
+})
+
+function handleChangeRecipe() {
+    const recipe_container = document.querySelector('.recipe__container');
+
+    const recipe_cards = document.querySelectorAll('.nutritional__recipe');
+    recipe_cards.forEach(recipe_card => {
+        recipe_card.addEventListener('click', () => {
+            const id = recipe_card.getAttribute('data-id');
+            const { image, title, description, ingredients, instructions } = getRecipeById(id - 1);
+
+            const recipe = `
+                <div class="recipe">
+                    <div class="recipe__image">
+                        <img src="${image}" alt="${title}">
+                    </div>
+                    <div class="recipe__content">
+                        <div class="recipe__text">
+                            <h3>${title}</h3>
+                            <p>${description}</p>
+                        </div>
+                        <div class="recipe__ingredients">
+                            <h4>Ingredientes</h4>
+                            <ul class="recipe__list">
+                                ${ingredients.map(ingredient => `<li><span>✓</span>${ingredient}</li>`).join('')}
+                            </ul>
+                        </div>
+                        <div class="recipe__instructions">
+                            <h4>Preparación</h4>
+                            <ul class="recipe__list">
+                             ${instructions.map((instruction, index) => `<li><span>${index + 1}.</span> ${instruction}</li>`).join('')}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            `
+
+            recipe_container.innerHTML = recipe
+        })
+    })
+}
--- resources/views/home/diet.blade.php
+++ resources/views/home/diet.blade.php
@@ -1,67 +1,29 @@
 <x-layouts.user-layout>
+  <section>
+    <header class="nutritional__header"
+      style="background-image: url({{ asset('assets/images/home/banner_comida.jpg') }});">
+      <div class="nutritional__header-text">
+        <h3>Recomendación Nutricional</h3>
+        <p>Te proporcionaremos recomendaciones nutricionales personalizadas para ayudarte a alcanzar tus objetivos de
+          salud sin restringir alimentos. Es esencial querer cambiar nuestros malos hábitos. Evita prácticas
+          perjudiciales como provocar vómitos, seguir dietas supervisión médica o utilizar productos dudosos en tu
+          rutina de ejercicio. <span>Recuerda, tu salud es lo más importante.</span>
+        </p>
+      </div>
+    </header>
+    <main class="nutritional__articles">
+      <h4>Recetas Saludables para ti</h4>
+      <p>No sabes que cocinar? No te preocupes, te damos algunas recomendaciones de comidas saludables.</p>
+      <div class="nutritional__button">
+        <button class="button button-primary" type="button" id="random-recipes">
+          Cambiar recetas
+        </button>
+      </div>
+      <ul class="nutritional__list"></ul>
+      <article class="recipe__container">
 
-    <section class="section__diet">
-
-        <div class="diet__header">
-            <img src="{{ asset('assets/images/home/banner_comida.jpg') }}" alt="Dieta">
-
-            <div class="diet__header-title">
-                <h2>Recomendación nutricional</h2>
-            </div>
-        </div>
-
-        <div class="diet__content">
-            <div class="diet__info">
-                <p>Te proporcionaremos recomendaciones nutricionales personalizadas para ayudarte a alcanzar tus
-                    objetivos de salud sin restringir alimentos. Es esencial querer cambiar nuestros malos hábitos.
-                    Evita prácticas perjudiciales como provocar vómitos, seguir dietas sin supervisión médica o utilizar
-                    productos dudosos en tu rutina de ejercicio.
-                    Recuerda, tu salud es lo más importante.</p>
-            </div>
-
-            <div class="diet__recipes">
-
-                <div class="recipe__card">
-                    <img src="{{ asset('assets/images/home/receta1.jpg') }}" alt="Dieta">
-                    <div class="recipe__text">
-                        <h3>Receta 1</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit accusamus temporibus
-                            voluptates exercitationem, ipsa officiis a animi esse explicabo repudiandae in.</p>
-                        <a href="">Ver receta</a>
-                    </div>
-                </div>
-
-                <div class="recipe__card">
-                    <img src="{{ asset('assets/images/home/receta2.jpg') }}" alt="Dieta">
-                    <div class="recipe__text">
-                        <h3>Receta 2</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit accusamus temporibus
-                            voluptates exercitationem, ipsa officiis a animi esse explicabo repudiandae in.</p>
-                        <a href="">Ver receta</a>
-                    </div>
-                </div>
-
-                <div class="recipe__card">
-                    <img src="{{ asset('assets/images/home/receta3.jpg') }}" alt="Dieta">
-                    <div class="recipe__text">
-                        <h3>Receta 3</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit accusamus temporibus
-                            voluptates exercitationem, ipsa officiis a animi esse explicabo repudiandae in.</p>
-                        <a href="">Ver receta</a>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="diet__info">
-                <p>No te desesperes ni te des por vencido si no observas resultados de forma inmediata, los primeros
-                    resultados se comienzan a observar después de 3 meses de esfuerzo continuo y disciplina.
-                    <br>
-                    Para establecer un nuevo hábito, necesitas al menos de 6 a 8 semanas de voluntad, perseverancia y
-                    disciplina. Después de este periodo, se volverá parte de tu vida diaria de manera más sencilla.</p>
-            </div>
-        </div>
-
-    </section>
-
+      </article>
+    </main>
+  </section>
+  <script src="{{ asset('js/home/diet.js') }}" type="module"></script>
 </x-layouts.user-layout>
