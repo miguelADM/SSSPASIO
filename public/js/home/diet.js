19c3b8bf5745586c13a45e412c8a82a0cf9fe092
baezdev <baezdev@gmail.com>
2023-11-05 22:49:11 -0600

    style: efecto a la tarjeta de receta

--- public/js/home/diet.js
+++ public/js/home/diet.js
@@ -1,5 +1,8 @@
+const nutritional__list = document.querySelector(".nutritional__list");
+const random_recipes_button = document.querySelector("#random-recipes");
+
 async function getRecipes() {
-    const response = await fetch('/data/recipes.json');
+    const response = await fetch("/data/recipes.json");
     const data = await response.json();
 
     return data;
@@ -9,66 +12,64 @@ const recipes = await getRecipes();
 
 const getRecipeById = (id) => {
     return recipes[id];
-}
+};
 
 const getRandomNumber = (max) => {
     return Math.floor(Math.random() * max);
-}
+};
 const getRandomRecipes = () => {
-    const max = recipes.length
+    const max = recipes.length;
 
     return [
         recipes[getRandomNumber(max)],
         recipes[getRandomNumber(max)],
-        recipes[getRandomNumber(max)]
-    ]
-}
-
-const nutritional__list = document.querySelector('.nutritional__list');
-const random_recipes_button = document.querySelector('#random-recipes');
+        recipes[getRandomNumber(max)],
+    ];
+};
 
 function createRecipeCard(recipe) {
-    const recipe_card = document.createElement('li');
-    recipe_card.classList.add('nutritional__recipe');
-    recipe_card.style.backgroundImage = `url(${recipe.image})`
-    recipe_card.setAttribute('data-id', recipe.id);
+    const recipe_card = document.createElement("li");
+    recipe_card.classList.add("nutritional__recipe");
+    recipe_card.style.setProperty("--bg-image", `url(${recipe.image})`);
+    recipe_card.setAttribute("data-id", recipe.id);
 
-    const recipe_text = document.createElement('div');
-    recipe_text.classList.add('nutritional__recipe-text');
+    const recipe_text = document.createElement("div");
+    recipe_text.classList.add("nutritional__recipe-text");
     recipe_text.innerHTML = `
         <h5>${recipe.title}</h5>
         <p></p>
-        <span>${recipe.description}</span>`
+        <span>${recipe.description}</span>`;
 
     recipe_card.appendChild(recipe_text);
-    return recipe_card
+    return recipe_card;
 }
 
 function generateRandomRecipes() {
     const randomRecipes = getRandomRecipes();
-    randomRecipes.forEach(recipe => {
+    randomRecipes.forEach((recipe) => {
         const recipe_card = createRecipeCard(recipe);
         nutritional__list.appendChild(recipe_card);
-    })
+    });
 
-    handleChangeRecipe()
+    handleChangeRecipe();
 }
 
-generateRandomRecipes()
+generateRandomRecipes();
 
-random_recipes_button.addEventListener('click', () => {
-    nutritional__list.innerHTML = '';
+random_recipes_button.addEventListener("click", () => {
+    nutritional__list.innerHTML = "";
     generateRandomRecipes();
-})
+});
 
 function handleChangeRecipe() {
-    const recipe_container = document.querySelector('.recipe__container');
+    const recipe_container = document.querySelector(".recipe__container");
 
-    const recipe_cards = document.querySelectorAll('.nutritional__recipe');
-    recipe_cards.forEach(recipe_card => {
-        recipe_card.addEventListener('click', () => {
-            const id = recipe_card.getAttribute('data-id');
-            const { image, title, description, ingredients, instructions } = getRecipeById(id - 1);
+    const recipe_cards = document.querySelectorAll(".nutritional__recipe");
+    recipe_cards.forEach((recipe_card) => {
+        recipe_card.addEventListener("click", () => {
+            const id = recipe_card.getAttribute("data-id");
+            const { image, title, description, ingredients, instructions } =
+                getRecipeById(id - 1);
 
             const recipe = `
                 <div class="recipe">
@@ -83,20 +84,31 @@ function handleChangeRecipe() {
                         <div class="recipe__ingredients">
                             <h4>Ingredientes</h4>
                             <ul class="recipe__list">
-                                ${ingredients.map(ingredient => `<li><span>✓</span>${ingredient}</li>`).join('')}
+                                ${ingredients
+                    .map(
+                        (ingredient) =>
+                            `<li><span>✓</span>${ingredient}</li>`
+                    )
+                    .join("")}
                             </ul>
                         </div>
                         <div class="recipe__instructions">
                             <h4>Preparación</h4>
                             <ul class="recipe__list">
-                             ${instructions.map((instruction, index) => `<li><span>${index + 1}.</span> ${instruction}</li>`).join('')}
+                             ${instructions
+                    .map(
+                        (instruction, index) =>
+                            `<li><span>${index + 1
+                            }.</span> ${instruction}</li>`
+                    )
+                    .join("")}
                             </ul>
                         </div>
                     </div>
                 </div>
-            `
+            `;
 
-            recipe_container.innerHTML = recipe
-        })
-    })
+            recipe_container.innerHTML = recipe;
+        });
+    });
 }
